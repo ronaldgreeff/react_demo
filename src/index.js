1f29ff338f7cfcb834b25eec9f4ed8de7f38a60b
Ron G <ronald.greeff@gmail.com>
2020-08-31 15:18:14 +0100

    state being passed to children

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  // renders a single <button>
+  // renders a single <button> element representing a square on the board
+
+  // lets store the current state of Square in this.state and change it
+  // when Square is clicked. To do so, first we add a constructor, then
+  //  we change the render method to display the current state
+  constructor(props) {
+    // always call super when defining the constructor of a subclass
+    // All React component classes that have a constructor should start
+    // with a super(props) call
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      //                            onClick={function() { alert('click');}
+      // <button className="square" onClick={() => alert(this.props.state)}>
+
+      // By calling this.setState from an onClick handler
+      // in the Square’s render method, we tell React to
+      // re-render that Square whenever its <button> is clicked.
+
+      // When you call setState in a component, React
+      // automatically updates the child components inside of it too.
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
       </button>
     );
   }
@@ -16,6 +41,9 @@ class Square extends React.Component {
 class Board extends React.Component {
   // renders 9x squares
   renderSquare(i) {
+    // pass prop from parent to child
+    // "Passing props is how information flows in React apps,
+    // from parents to children."
     return <Square value={i}/>;
   }
 
